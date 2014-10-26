b866e0e3070e07fe757ec9ed199995bb96f83d24
ggobbe <guillaume.gobbe@gmail.com>
2014-10-26 22:12:46 +0000

    Improvements on the building of the adjacents map

--- ex1.go
+++ ex1.go
@@ -22,20 +22,36 @@ func numberOfAdjacentPairs(a []int) int {
 	adj := make(map[int]*Node)
 
 	for i := 0; i < len(a); i++ {
+		if _, ok := adj[a[i]]; !ok {
+			adj[a[i]] = &Node{a[i], a[i], a[i]}
+		}
+
 		for j := i + 1; j < len(a); j++ {
+			if _, ok := adj[a[j]]; !ok {
+				adj[a[j]] = &Node{a[j], a[j], a[j]}
+			}
+
+			if a[i] > a[j] && (adj[a[i]].Low < a[j] || adj[a[i]].Low == a[i]) {
+				adj[a[i]].Low = a[j]
+				adj[a[j]].High = a[i]
+			}
+
+			if a[i] < a[j] && (adj[a[i]].High > a[j] || adj[a[i]].High == a[i]) {
+				adj[a[i]].High = a[j]
+				adj[a[j]].Low = a[i]
+			}
+
 			if math.Abs(float64(a[i]-a[j])) > 1 {
 				num++
 			} else {
-				if _, ok := adj[a[i]]; !ok {
-					adj[a[i]] = &Node{a[i], a[i], a[i]}
-				}
-				if _, ok := adj[a[j]]; !ok {
-					adj[a[j]] = &Node{a[j], a[j], a[j]}
-				}
 				num++
 				fmt.Printf("Adjacent pair: a[%d]=%d - a[%d]=%d\n", i, a[i], j, a[j])
 			}
 		}
+	}
+
+	for key, value := range adj {
+		fmt.Printf("[%d]Node %d [%d, %d]\n", key, value.Value, value.Low, value.High)
 	}
 
 	return num
