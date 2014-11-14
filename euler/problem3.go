22f318ef162317b97a94b2907f919587a3289d8e
ggobbe <guillaume.gobbe@gmail.com>
2014-11-14 09:57:39 +0000

    Solution to Project Euler problem 3

---
+++ euler/problem3.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Answer:", largestPrimeFactor(600851475143))
+}
+
+func largestPrimeFactor(n int) int {
+	var largest int = 1
+	for i := 2; n > 1; i++ {
+		for n%i == 0 {
+			if i > largest {
+				largest = i
+			}
+			n /= i
+		}
+	}
+	return largest
+}
