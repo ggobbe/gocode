27e71cb598a2b6366311d91244df8191cdfc82d2
ggobbe <guillaume.gobbe@gmail.com>
2014-11-22 12:18:11 +0000

    Solution to Project Euler problem 9

--- euler/problem9.go
+++ euler/problem9.go
@@ -1,5 +1,14 @@
 package main
 
 func problem9() int {
+	for a := 1; a <= 997; a++ {
+		for b := a + 1; b <= 997; b++ {
+			for c := b + 1; c <= 997; c++ {
+				if a+b+c == 1000 && (a*a)+(b*b) == (c*c) {
+					return a * b * c
+				}
+			}
+		}
+	}
 	return 0
 }
